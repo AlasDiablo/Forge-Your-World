ad11ee0e285868494c02078a8a2e1d51e33591f8
AlasDiablo <marcolet.pierre@laposte.net>
2017-06-25 19:58:45 +0200

    Create ItemFlour.java

---
+++ src/main/java/alasdiablo_rom279/forgeworld/items/food/ItemFlour.java
@@ -0,0 +1,13 @@
+package alasdiablo_rom279.forgeworld.items.food;
+
+import alasdiablo_rom279.forgeworld.Reference;
+import net.minecraft.item.Item;
+
+public class ItemFlour extends Item {
+	
+	public ItemFlour() {
+		setUnlocalizedName(Reference.FWItems.FLOUR.getUnlocalizedName());
+		setRegistryName(Reference.FWItems.FLOUR.getRegistryName());
+	}
+
+}
