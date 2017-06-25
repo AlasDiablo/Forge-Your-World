8afd1d4292bf19cb9074fa52a08f1f0dd7ea23a4
AlasDiablo <marcolet.pierre@laposte.net>
2017-06-25 19:56:50 +0200

    Update inititems.java

--- src/main/java/alasdiablo_rom279/forgeworld/init/inititems.java
+++ src/main/java/alasdiablo_rom279/forgeworld/init/inititems.java
@@ -1,23 +1,30 @@
 package alasdiablo_rom279.forgeworld.init;
 
+import alasdiablo_rom279.forgeworld.modinfo;
+import alasdiablo_rom279.forgeworld.items.food.ItemFlour;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class inititems {
 	
+	public static Item flour;
+	
 	public static void init() {
-		
+		flour = new ItemFlour();
 	}
 	
 	public static void register() {
-		
+		GameRegistry.register(flour);
 	}
 	
 	public static void registerRenders() {
-		
+		registerRender(flour);
 	}
 	
 	private static void registerRender(Item item) {
-		
+		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0, new ModelResourceLocation(modinfo.MOD_ID + ":" + item.getUnlocalizedName().substring(5), "inventory"));
 	}
 
 }
