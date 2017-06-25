577fb7a2e48f74cf5263f1ed123f06b5d914b937
AlasDiablo <marcolet.pierre@laposte.net>
2017-06-25 20:30:38 +0200

    Update client.java

--- src/main/java/alasdiablo_rom279/forgeworld/proxy/client.java
+++ src/main/java/alasdiablo_rom279/forgeworld/proxy/client.java
@@ -1,5 +1,12 @@
 package alasdiablo_rom279.forgeworld.proxy;
 
+import alasdiablo_rom279.forgeworld.init.inititems;
+
 public class client implements common{
+
+	@Override
+	public void init() {
+		inititems.registerRenders();
+	}
 
 }
