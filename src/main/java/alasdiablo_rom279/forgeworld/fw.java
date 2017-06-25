b16d9cd83fb8f4cd2ecbdd8a3fc9129549c01b2f
AlasDiablo <marcolet.pierre@laposte.net>
2017-06-25 18:31:12 +0200

    Update fw.java

--- src/main/java/alasdiablo_rom279/forgeworld/fw.java
+++ src/main/java/alasdiablo_rom279/forgeworld/fw.java
@@ -1,7 +1,39 @@
 package alasdiablo_rom279.forgeworld;
 
-@Mod(modid = )
+import alasdiablo_rom279.forgeworld.proxy.common;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
+@Mod(modid = modinfo.MOD_ID, name = modinfo.NAME, version = modinfo.VERSION, acceptedMinecraftVersions = modinfo.ACCEPTED_VERSION)
 public class fw {
+	
+	@Instance
+	public static fw instance;
+	
+	@SidedProxy(clientSide = modinfo.CLIENT_PROXY_CLASSE, serverSide = modinfo.SERVER_PROXY_CLASSE)
+	public static common proxy;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		System.out.println("Pre Initialization of Forge Your World Done");
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		System.out.println("Initialization of Forge Your World Done");
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		System.out.println("Post Initialization of Forge Your World Done");
+	}
 
 }
