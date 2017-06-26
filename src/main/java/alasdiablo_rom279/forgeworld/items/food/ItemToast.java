32a4678e053761677d801d94f4373c877d35e5a2
AlasDiablo <marcolet.pierre@laposte.net>
2017-06-26 13:33:10 +0200

    Create ItemToast.java

---
+++ src/main/java/alasdiablo_rom279/forgeworld/items/food/ItemToast.java
@@ -0,0 +1,13 @@
+package alasdiablo_rom279.forgeworld.items.food;
+
+import alasdiablo_rom279.forgeworld.Reference;
+import net.minecraft.item.Item;
+
+public class ItemToast extends Item {
+	
+	public ItemToast() {
+		setUnlocalizedName(Reference.FWItems.TOAST.getUnlocalizedName());
+		setRegistryName(Reference.FWItems.TOAST.getRegistryName());
+	}
+
+}
